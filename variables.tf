b7a3e0db20ec0c31c5d44888992b8af41641bcc1
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 14:04:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
+# Define the bucket_name variable
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
 
+# Define the region variable (region will be managed by the provider)
 variable "region" {
   description = "AWS region"
   type        = string
