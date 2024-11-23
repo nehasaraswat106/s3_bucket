54490cb445726f3423a21b5d71fa9c041e788c98
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 11:07:53 +0530

    Update and rename var.tf to variables.tf

--- var.tf
+++
@@ -1,5 +0,0 @@
-variable "buckett_name" {
-    description = "my_s3_bucket"
-    type = string
-    default = "bucketnamebckt"
-}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
