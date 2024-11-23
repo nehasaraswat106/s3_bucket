c8aef70b82f49f71337cb33b5cfa44ab433f9adc
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:20:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-# S3 Bucket resource without the deprecated acl argument
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
-  region = var.region
-  # Remove acl argument
 }
 
 # Define acl in a separate aws_s3_bucket_acl resource
 resource "aws_s3_bucket_acl" "example_acl" {
   bucket = aws_s3_bucket.s3_bucket.bucket
-  acl    = "private"  # or another appropriate ACL
+  acl    = "private"  # or "public-read", depending on your needs
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.s3_bucket.arn
 }
