cad7857c2c0a3d862da2cb684cbf3fe76ffc41d5
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:50:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,3 @@ resource "aws_s3_bucket_acl" "example_acl" {
   bucket = aws_s3_bucket.s3_bucket.bucket
   acl    = "private"  # or "public-read", depending on your needs
 }
-
-output "bucket_arn" {
-  value = aws_s3_bucket.s3_bucket.arn
-}
