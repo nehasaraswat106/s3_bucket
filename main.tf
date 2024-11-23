9c7ba27ed3ccdcefa07934dc3580a725da03c5e2
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 14:10:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Declare the S3 bucket resource
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
@@ -9,12 +8,10 @@ resource "aws_s3_bucket_acl" "example_acl" {
   acl    = "private"  # You can change to "public-read" if needed
 }
 
-# Output the bucket name
 output "bucket_name" {
   value = aws_s3_bucket.s3_bucket.bucket
 }
 
-# Output the bucket ARN
 output "bucket_arn" {
   value = aws_s3_bucket.s3_bucket.arn
 }
