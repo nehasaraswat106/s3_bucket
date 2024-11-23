d2687ca6ddb193ddc3de8ad2b3052fb821dc917d
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:58:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-provider "aws" {
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
   region = var.region
 }
+
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
