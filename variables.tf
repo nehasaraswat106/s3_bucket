0edbc3a00c9bec732f42945c87967758308f797e
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 11:31:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
+  default     = "my_bucket"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
