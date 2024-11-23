963ebbc88eaf4f8647050b4d0a36ff0e6b25e401
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:11:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-# In your S3 bucket module (main.tf)
-resource "aws_s3_bucket" "example" {
+# S3 Bucket resource without the deprecated acl argument
+resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   region = var.region
-  # Remove the acl argument
+  # Remove acl argument
 }
 
-# Define the acl in a separate aws_s3_bucket_acl resource
+# Define acl in a separate aws_s3_bucket_acl resource
 resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.bucket
-  acl    = "private"
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  acl    = "private"  # or another appropriate ACL
 }
