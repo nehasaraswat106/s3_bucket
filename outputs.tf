b03a2b003fb947e19765f7143771c6d8d18019c9
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:12:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+# Correct reference to the s3 bucket resource
 output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
+  value = aws_s3_bucket.s3_bucket.arn  # Reference the correct resource name
 }
